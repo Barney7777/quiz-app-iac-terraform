ac92ed8ef4097dbd2d376042e731d3891f584efd
Barney Wang <wangyaxu7@gmail.com>
2024-04-26 20:18:00 +1000

    update workflow

--- backend-iac/backend.tf
+++ backend-iac/backend.tf
@@ -4,6 +4,6 @@ terraform {
     bucket         = "quiz-app-terraform-remote-state"
     key            = "backend/dev/terraform.tfstate"
     region         = "ap-southeast-2"
-    dynamodb_table = "quiz-app-backend-terraform-state-lock"
+    # dynamodb_table = "quiz-app-backend-terraform-state-lock"
   }
 }
