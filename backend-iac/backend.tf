0a0b7eb13e0abf3e588711a344e89b5c69e9ddfc
Barney Wang <wangyaxu7@gmail.com>
2024-04-28 22:46:14 +1000

    update workflow

--- backend-iac/backend.tf
+++ backend-iac/backend.tf
@@ -1,9 +1,9 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket = "quizapp-terraform-remote-state"
-    key    = "backend/dev/terraform.tfstate"
-    region = "ap-southeast-2"
+    bucket         = "quizapp-terraform-remote-state"
+    key            = "backend/dev/terraform.tfstate"
+    region         = "ap-southeast-2"
     dynamodb_table = "quizapp-backend-terraform-state-lock"
   }
 }
